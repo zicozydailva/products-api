8d06e0befbbccad4b369c8ede0abcccf0fc5b4df
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-03-26 21:23:09 +0100

    feat: products controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,7 +1,71 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/product.dto';
 
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async create(@Body() createProductDto: CreateProductDto) {
+    const res = await this.productService.create(createProductDto);
+
+    return {
+      data: res,
+      message: 'Product Created Successfully',
+      success: true,
+    };
+  }
+
+  @Get()
+  async findAll() {
+    const res = await this.productService.findAll();
+
+    return {
+      data: res,
+      message: 'Products Fetched Successfully',
+      success: true,
+    };
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const res = await this.productService.findOne(id);
+
+    return {
+      data: res,
+      message: 'Product Fetched Successfully',
+      success: true,
+    };
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateProductDto) {
+    const res = await this.productService.update(id, updateProductDto);
+
+    return {
+      data: res,
+      message: 'Product Updated Successfully',
+      success: true,
+    };
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    const res = await this.productService.remove(id);
+
+    return {
+      data: res,
+      message: 'Product Deleted Successfully',
+      success: true,
+    };
+  }
 }
