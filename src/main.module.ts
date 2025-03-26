0b1c78c0bf8ad5c866f721366b06f89c20819a63
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-03-26 21:24:14 +0100

    feat: dependency injection - prod svc

--- src/main.module.ts
+++ src/main.module.ts
@@ -5,12 +5,14 @@ import { SecretsModule } from './global/secrets/module';
 import { SecretsService } from './global/secrets/service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
   imports: [
     GlobalModule,
     AuthModule,
     UserModule,
+    ProductModule,
     MongooseModule.forRootAsync({
       imports: [SecretsModule],
       inject: [SecretsService],
--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schema/product.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   controllers: [ProductController],
   providers: [ProductService],
 })
