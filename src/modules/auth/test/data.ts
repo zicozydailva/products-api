93364cb6e89e77eec1fb125aa97b862b47d6e6e8
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-03-27 09:00:55 +0100

    chore: unit test setup

---
+++ src/modules/auth/test/data.ts
@@ -0,0 +1,8 @@
+import { CreateUserDto } from 'src/modules/user/dto/user.dto';
+
+export const testUser: CreateUserDto = {
+  email: 'test@darey.io',
+  firstName: 'test',
+  lastName: 'test',
+  password: 'test',
+};
